d8733935659d8821f9e5abc57f6d9d10901c43cc
web-ali <suleymanov.web@gmail.com>
2024-11-05 08:22:42 +0300

    fix: подключил апишку

--- src/core/layout/layout.tsx
+++ src/core/layout/layout.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable no-restricted-imports */
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
-import { useScrollToTop } from "../../hooks/scroll-to-top";
-import { Header } from "../header";
+import { useScrollToTop } from '../../hooks/scroll-to-top';
+import { Header } from '../header';
 
 export function Layout() {
-  useScrollToTop()
-   
-    return (
-      <div className="pb-10">
-        <Header />
-        <Outlet />
+  useScrollToTop();
+
+  return (
+    <div className="">
+      <Header />
+      <Outlet />
+      <div
+        style={{ boxShadow: '0px 2px 15.8px 0px #00000040' }}
+        className="mt-20 py-6 text-center text-lg text-my-red"
+      >
+        © 2024 ГРОЗНЕНСКИЙ ГОСУДАРСТВЕННЫЙ НЕФТЯНОЙ ТЕХНИЧЕСКИЙ УНИВЕРСИТЕТ
       </div>
-    );
-  }
+    </div>
+  );
+}
